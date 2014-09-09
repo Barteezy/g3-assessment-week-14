f4642b8092c34b37301a7ad9e8c9dde251e1ed25
Luke Bartel <bartelluke@gmail.com>
2014-09-08 22:20:51 -0600

    dosage must start with an integer

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -6,11 +6,19 @@ class Prescription < ActiveRecord::Base
   validates :ends, presence: true
 
   validate :starts_before_ends
+  validate :dosage_starts_with_integer
 
 
   def starts_before_ends
     if :starts > :ends
       errors.add(:starts, "can't be after ends")
+    end
+  end
+
+  def dosage_starts_with_integer
+
+    if self.dosage.index(/^\d+\s/) == nil
+      errors.add(:dosage, "must start with an integer")
     end
   end
 end
